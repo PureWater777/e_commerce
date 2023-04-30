7ecce4c0ed9f256dfd1af61b0282c4da00401997
PureWater <piotr.adam.kuzma@gmail.com>
2023-04-30 13:06:22 +0200

    Update admin

--- store/admin.py
+++ store/admin.py
@@ -68,9 +68,10 @@ class CollectionAdmin(admin.ModelAdmin):
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ["first_name", "last_name", "membership", "orders_count"]
-    ordering = ["first_name", "last_name"]
+    ordering = ["user__first_name", "user__last_name"]
     list_editable = ["membership"]
-    search_fields = ["first_name__istartswith", "last_name__istartswith"]
+    list_select_related = ["user"]
+    search_fields = ["user__first_name__istartswith", "user__last_name__istartswith"]
 
     @admin.display(ordering="orders_count")
     def orders_count(self, customer):
